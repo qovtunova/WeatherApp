cd6c58b9c70ac821922572b8f1f39a6b67eafa8c
Oleksandra <qovtun>
2024-10-21 17:23:54 +0200

    Initial Commit

---
+++ WeatherTests/WeatherTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherTests.swift
+//  WeatherTests
+//
+//  Created by Oleksandra Kovtunova on 21/10/2024.
+//
+
+import Testing
+@testable import Weather
+
+struct WeatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
